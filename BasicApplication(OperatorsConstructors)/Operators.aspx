5867ce73865dbbff57a6c6722ebbad77310293a6
khadarvalishiak <khadarshaik91@gmail.com>
2018-02-27 22:34:12 +0530

    Add files via upload

---
+++ BasicApplication(OperatorsConstructors)/Operators.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Operators.aspx.cs" Inherits="BasicApplication_Operators" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+
+</head>
+   
+<body>
+    <form id="form1" runat="server">
+       
+        <div id="Validate" runat="server">
+
+            <asp:Label runat="server" Text="Enter UserName" />
+
+            <asp:TextBox runat="server" ID="TextBox1"  ></asp:TextBox><br />
+           <asp:Button runat="server" ID="GetUserName"  Text="click Me" OnClick="GetUserName_Click"/>
+        </div>
+        <div>
+     <h2 id="bindUser" runat="server">
+
+     </h2>
+
+           
+            <asp:Label runat="server" Text="Enter A value" />
+            <asp:TextBox runat="server" ID="AValue" ></asp:TextBox><br />
+                <asp:Label runat="server" Text="Enter B value" />
+            <asp:TextBox runat="server" ID="BValue"  ></asp:TextBox><br />
+            <asp:Button runat="server" ID="Caluculate"  Text="Add" OnClick="Caluculate_Click"/>
+           
+            <asp:Button runat="server" ID="sub"  Text="sub" OnClick="sub_Click"/>
+            <asp:Button runat="server" ID="mul"  Text="mul" OnClick="mul_Click"/>
+            <asp:Button runat="server" ID="div"  Text="div" OnClick="div_Click"/>
+            <asp:Button runat="server" ID="modules"  Text="modules" OnClick="modules_Click"/>
+            <h4 id="result" runat="server">
+
+            </h4>
+        </div>
+    </form>
+</body>
+</html>
---
+++ BasicApplication(OperatorsConstructors)/Operators.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class BasicApplication_Operators : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //  Validate.InnerText
+        if(!IsPostBack)
+        { 
+            //By default Constructor will be called Here.
+        GetValue sendName = new GetValue();
+        bindUser.InnerText = sendName.userName;
+        }
+    }
+
+    protected void Caluculate_Click(object sender, EventArgs e)
+    {
+        int c= Convert.ToInt32(AValue.Text) + Convert.ToInt32(BValue.Text);
+        result.InnerText ="The value of A + b is "+  c.ToString();
+
+    }  
+
+    protected void sub_Click(object sender, EventArgs e)
+    {
+        int c = Convert.ToInt32(AValue.Text) - Convert.ToInt32(BValue.Text);
+        result.InnerText = "The value of A - b is " + c.ToString();
+    }
+
+    protected void mul_Click(object sender, EventArgs e)
+    {
+        int c = Convert.ToInt32(AValue.Text) * Convert.ToInt32(BValue.Text);
+        result.InnerText = "The value of A * b is " + c.ToString();
+    }
+
+    protected void div_Click(object sender, EventArgs e)
+    {
+        int c = Convert.ToInt32(AValue.Text) / Convert.ToInt32(BValue.Text);
+        result.InnerText = "The value of A / b is " + c.ToString();
+    }
+
+    protected void modules_Click(object sender, EventArgs e)
+    {
+        int c = Convert.ToInt32(AValue.Text) % Convert.ToInt32(BValue.Text);
+        result.InnerText = "The value of A % b is " + c.ToString();
+    }
+
+    protected void GetUserName_Click(object sender, EventArgs e)
+    {
+
+        // GetValue sendName = new GetValue(TextBox1.Text); //parameterised constructor will be invocekd here.
+        //bindUser.InnerText = sendName.userName;
+
+
+        //Seting like userDefined
+        GetValue sn = new GetValue();
+        sn.setName(TextBox1.Text);
+        bindUser.InnerText = sn.userName;
+
+    }
+}
