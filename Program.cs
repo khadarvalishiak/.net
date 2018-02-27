870eb073f32240d50f02c5cd75d6c7cb48d1e342
khadarvalishiak <khadarshaik91@gmail.com>
2018-02-27 23:37:07 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CSharpBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int id=10;//takes 4 bytes of memory
+            string Name = "Khadar";//takes the string Length
+            long MobileNumber = 8143989689;
+            float salary = 20000.3f;
+            bool flag = true;
+            Console.WriteLine("EmployeeId :" +id  +"\n "  +"EmployeeName:" +Name +"\n" +"Employee Mobile Number :" + MobileNumber + "\n" +"Employee Salary"+salary+"\n");
+           
+            Console.ReadLine();
+        }
+    }
+}
